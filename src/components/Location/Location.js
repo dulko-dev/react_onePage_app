b8e89f2e1fc2a4811c0a9add1b9725eba3b818bb
Kamil Duliniec <123dulko@wp.pl>
2021-10-18 20:32:01 +0200

    initial geolocation and style location component

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -3,10 +3,44 @@ import Home from "../Home";
 import styles from "../../style/Location.module.css";
 
 const Location = () => {
+  const getLocation = (e) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+
+      function error(err) {
+        if (err.code === 1) {
+          e.target.innerText = err.message;
+        } else if (err.code === 2) {
+          e.target.innerText = "Location not available";
+        } else {
+          e.target.innerText = "Something gone wrong";
+        }
+      }
+
+      function success(pos) {
+        const time = pos.timestamp;
+        const setTime = new Date(time).toLocaleTimeString("en-US");
+        const setDate = new Date(time).toLocaleDateString("pl-PL");
+        console.log(setTime, setDate);
+        const { latitude, longitude } = pos.coords;
+        console.log(latitude, longitude);
+      }
+    } else {
+      e.target.innerText = "Your browser doesn't support geolocation";
+    }
+  };
+
   return (
     <div className={styles.location}>
       <Home />
-      <div>Location</div>
+      <div className={styles.content}>
+        <div className={styles.buttonLocation}>
+          <button type="button" className={styles.button} onClick={getLocation}>
+            Detect your Location
+          </button>
+        </div>
+        <div className={styles.map}>Here will be map...</div>
+      </div>
     </div>
   );
 };
