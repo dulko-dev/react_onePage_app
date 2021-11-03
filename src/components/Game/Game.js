d6b75e50a7d72e3c264ebf4bc8b0c08af75033fc
Kamil Duliniec <123dulko@wp.pl>
2021-11-03 17:07:00 +0100

    set promise.all and display static question on card game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "../Home";
 import styles from "../../style/Game.module.css";
 
 const Game = () => {
+  const [data, setData] = useState("");
+
+  const handleFetch = async () => {
+    const url1 =
+      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
+    const url2 =
+      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
+    const url3 =
+      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
+    const url4 =
+      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
+
+    const promiseAll = await Promise.all([
+      fetch(url1),
+      fetch(url2),
+      fetch(url3),
+      fetch(url4),
+    ]);
+    const data = await promiseAll.map((data) => data.json());
+    const final = Promise.all(data);
+    final.then((el) => setData(el));
+  };
+
   return (
     <div className={styles.game}>
       <Home />
@@ -15,27 +38,32 @@ const Game = () => {
         </div>
         <div className={styles.questionContent}>
           <div className={styles.questionRow}>
-            <div className={styles.question}>100</div>
-            <div className={styles.question}>200</div>
-            <div className={styles.question}>300</div>
+            {data &&
+              data[0].results.map((question) => (
+                <div className={styles.question}>{question.question}</div>
+              ))}
           </div>
           <div className={styles.questionRow}>
-            <div className={styles.question}>100</div>
-            <div className={styles.question}>200</div>
-            <div className={styles.question}>300</div>
+            {data &&
+              data[1].results.map((question) => (
+                <div className={styles.question}>{question.question}</div>
+              ))}
           </div>
           <div className={styles.questionRow}>
-            <div className={styles.question}>100</div>
-            <div className={styles.question}>200</div>
-            <div className={styles.question}>300</div>
+            {data &&
+              data[2].results.map((question) => (
+                <div className={styles.question}>{question.question}</div>
+              ))}
           </div>
           <div className={styles.questionRow}>
-            <div className={styles.question}>100</div>
-            <div className={styles.question}>200</div>
-            <div className={styles.question}>300</div>
+            {data &&
+              data[3].results.map((question) => (
+                <div className={styles.question}>{question.question}</div>
+              ))}
           </div>
         </div>
       </div>
+      <button onClick={handleFetch}>Button</button>
     </div>
   );
 };
