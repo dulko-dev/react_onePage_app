5454253b64e1448bb4cee6ecba4b4cb819e18cb2
Kamil Duliniec <123dulko@wp.pl>
2021-11-04 13:22:47 +0100

    divide display component in games for menu and game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,69 +1,16 @@
 import React, { useState } from "react";
 import Home from "../Home";
+import Menu from "./Menu";
+import Live from "./Live";
 import styles from "../../style/Game.module.css";
 
 const Game = () => {
-  const [data, setData] = useState("");
-
-  const handleFetch = async () => {
-    const url1 =
-      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
-    const url2 =
-      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
-    const url3 =
-      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
-    const url4 =
-      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
-
-    const promiseAll = await Promise.all([
-      fetch(url1),
-      fetch(url2),
-      fetch(url3),
-      fetch(url4),
-    ]);
-    const data = await promiseAll.map((data) => data.json());
-    const final = Promise.all(data);
-    final.then((el) => setData(el));
-  };
+  const [welcomeScreen, setWelcomeScreen] = useState(true);
 
   return (
     <div className={styles.game}>
       <Home />
-      <div className={styles.gameContent}>
-        <div className={styles.categoryContent}>
-          <div className={styles.category}>Chemia</div>
-          <div className={styles.category}>Prawo</div>
-          <div className={styles.category}>Gospodarka</div>
-          <div className={styles.category}>Sport</div>
-        </div>
-        <div className={styles.questionContent}>
-          <div className={styles.questionRow}>
-            {data &&
-              data[0].results.map((question) => (
-                <div className={styles.question}>{question.question}</div>
-              ))}
-          </div>
-          <div className={styles.questionRow}>
-            {data &&
-              data[1].results.map((question) => (
-                <div className={styles.question}>{question.question}</div>
-              ))}
-          </div>
-          <div className={styles.questionRow}>
-            {data &&
-              data[2].results.map((question) => (
-                <div className={styles.question}>{question.question}</div>
-              ))}
-          </div>
-          <div className={styles.questionRow}>
-            {data &&
-              data[3].results.map((question) => (
-                <div className={styles.question}>{question.question}</div>
-              ))}
-          </div>
-        </div>
-      </div>
-      <button onClick={handleFetch}>Button</button>
+      {welcomeScreen ? <Menu /> : <Live />}
     </div>
   );
 };
---
+++ src/components/Game/Live.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import styles from "../../style/Live.module.css";
+
+const Game = () => {
+  const [data, setData] = useState("");
+
+  const handleFetch = async () => {
+    const url1 =
+      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
+    const url2 =
+      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
+    const url3 =
+      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
+    const url4 =
+      "https://opentdb.com/api.php?amount=3&category=17&difficulty=medium&type=boolean";
+
+    const promiseAll = await Promise.all([
+      fetch(url1),
+      fetch(url2),
+      fetch(url3),
+      fetch(url4),
+    ]);
+    const data = await promiseAll.map((data) => data.json());
+    const final = Promise.all(data);
+    final.then((el) => setData(el));
+  };
+
+  return (
+    <div className={styles.gameLive}>
+      <div className={styles.gameContent}>
+        <div className={styles.categoryContent}>
+          <div className={styles.category}>Chemia</div>
+          <div className={styles.category}>Prawo</div>
+          <div className={styles.category}>Gospodarka</div>
+          <div className={styles.category}>Sport</div>
+        </div>
+        <div className={styles.questionContent}>
+          <div className={styles.questionRow}>
+            {data &&
+              data[0].results.map((question) => (
+                <div className={styles.question}>{question.question}</div>
+              ))}
+          </div>
+          <div className={styles.questionRow}>
+            {data &&
+              data[1].results.map((question) => (
+                <div className={styles.question}>{question.question}</div>
+              ))}
+          </div>
+          <div className={styles.questionRow}>
+            {data &&
+              data[2].results.map((question) => (
+                <div className={styles.question}>{question.question}</div>
+              ))}
+          </div>
+          <div className={styles.questionRow}>
+            {data &&
+              data[3].results.map((question) => (
+                <div className={styles.question}>{question.question}</div>
+              ))}
+          </div>
+        </div>
+      </div>
+      <button onClick={handleFetch}>Button</button>
+    </div>
+  );
+};
+
+export default Game;
