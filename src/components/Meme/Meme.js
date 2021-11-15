5019efb78d40ca34f5acb80fd606ebcd85170694
Kamil Duliniec <123dulko@wp.pl>
2021-11-15 13:01:39 +0100

    add structure for meme component

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,12 +1,35 @@
 import React from "react";
 import Home from "../Home";
+import Canvas from "./Canvas";
 import styles from "../../style/Meme.module.css";
 
 const Meme = () => {
+  const addObject = (e) => {
+    const url = URL.createObjectURL(e.target.files[0]);
+    console.log(url);
+  };
+
   return (
     <div className={styles.meme}>
       <Home />
-      <div>Meme</div>
+      <div className={styles.container}>
+        <div className={styles.contentText}>
+          <div className={styles.setupCanvas}>
+            <h2>Create your own MEME</h2>
+            <form>
+              <label htmlFor="imageFiles">Load image</label>
+              <input type="file" id="imageFiles" onChange={addObject} />
+              <label htmlFor="topText">Top Label Text</label>
+              <input id="topText" />
+              <label htmlFor="bottomText">Bottom Label Text</label>
+              <input id="bottomText" />
+            </form>
+          </div>
+        </div>
+        <div className={styles.imageCanvas}>
+          <Canvas />
+        </div>
+      </div>
     </div>
   );
 };
