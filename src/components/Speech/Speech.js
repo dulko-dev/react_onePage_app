be6834d08d6b8ba6ba495f0e66fcfc1d1a3e5a2d
Kamil Duliniec <123dulko@wp.pl>
2021-11-29 15:38:15 +0100

    style and done rwd for speech component

--- src/components/Speech/Speech.js
+++ src/components/Speech/Speech.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Home from "../Home";
-
 import styles from "../../style/Speech.module.css";
 
 const Speech = () => {
@@ -44,37 +43,38 @@ const Speech = () => {
   return (
     <div className={styles.speech}>
       <Home />
-      <h2 className={styles.title}>
-        Your personal <br /> text speech reader
-      </h2>
-      <div className={styles.content}>
-        <div className={styles.leftWing}>
-          <textarea
-            value={textareaValue}
-            onChange={onChangeText}
-            placeholder="enter your text"
-          ></textarea>
-        </div>
-        <div className={styles.rightWing}>
-          <div className={styles.rightContent}>
-            <div className={styles.selected}>
-              <h3>Choose Voicer</h3>
-              <select onChange={onChangeVoicer}>
-                {voices.length &&
-                  voices.map((voice, index) => (
-                    <option key={index} value={voice.name}>
-                      {voice.name}
-                    </option>
-                  ))}
-              </select>
+      <div className={styles.containerSpeech}>
+        <h2 className={styles.title}>
+          Your personal <br /> text speech reader
+        </h2>
+        <div className={styles.contentSpeech}>
+          <div className={styles.leftWing}>
+            <textarea
+              value={textareaValue}
+              onChange={onChangeText}
+              placeholder="enter your text"
+            ></textarea>
+          </div>
+          <div className={styles.rightWing}>
+            <div className={styles.rightContent}>
+              <div className={styles.selected}>
+                <h3>Choose Voicer</h3>
+                <select onChange={onChangeVoicer}>
+                  {voices.length &&
+                    voices.map((voice, index) => (
+                      <option key={index} value={voice.name}>
+                        {voice.name}
+                      </option>
+                    ))}
+                </select>
+              </div>
+              <button className={styles.btn} onClick={getText}>
+                Read Text
+              </button>
             </div>
-            <button className={styles.btn} onClick={getText}>
-              Read Text
-            </button>
           </div>
         </div>
       </div>
-      ;
     </div>
   );
 };
